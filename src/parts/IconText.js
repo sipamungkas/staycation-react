74c8ccf16f7a2a69affeffb5c8410a2028f258c2
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2020-12-03 22:32:59 +0700

    feat(parts):add IcontText element

---
+++ src/parts/IconText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+
+export default function IconText() {
+    return (
+        <Button className="brand-text-icon" type="link" href="">
+            Stay<span className="text-gray-900">cation</span>
+        </Button>
+    )
+}
+
